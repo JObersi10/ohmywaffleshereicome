2e6a5e094f3ca26df5dc0d230609d45ddbca6937
JObersi10 <68297002+JObersi10@users.noreply.github.com>
2026-03-22 22:09:56 +0000

    bvbasically new files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ohmywaffleshereicome
+c# ohmywaffleshereicome
 :3
+
+The sole purpose of this site is to show that i am not a bum and can code JS, HTML, & CSS. to be able to run the waffles program for our HackClub.
---
+++ script.js
@@ -0,0 +1,33 @@
+const titleElement = document.getElementById("mainTitle");
+
+function greet() {
+    alert("You actually clicked it. Hello.");
+}
+
+function updateStyle() {
+    titleElement.style.color = "lime";
+    titleElement.style.fontSize = "50px";
+    titleElement.style.backgroundColor = "black";
+    titleElement.innerHTML = "LAB OVERHAULED";
+}
+
+function handleInput() {
+    let input = document.getElementById("userInput").value;
+    let display = document.getElementById("displayArea");
+
+    if (input === "") {
+        display.innerHTML = "I said TYPE SOMETHING. It's empty.";
+        display.style.color = "red";
+    } else {
+        display.innerHTML = "Your soul '" + input + "' has been sent. Goodbye.";
+        display.style.color = "blue";
+    }
+}
+
+function runLoop() {
+    console.log("spamming job working rn...");
+    for (let i = 0; i < 10; i++) {
+        console.log("el temper di yo " + i);
+    }
+    alert("Check the console (F12) if you care.");
+}
